cf683c5e2f03ddb21b7ef88b24f1ed2f132bff5b
Dayu Liu <36178461+LDY681@users.noreply.github.com>
2021-08-25 00:40:26 +0800

    Sanguosha-League-Table
    Basic page layout and table

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="三国杀国战计分器"
+          class="shrink"
+          contain
+          min-width="80"
+          src="https://www.sanguosha.com/static/pc/dist/img/logo.png?v=20200417"
+          width="80"
+        />
+      </div>
+      <v-spacer></v-spacer>
+      <v-btn class="mx-2" dark color="yellow darken-1">
+        <v-icon left dark>
+          mdi-pencil
+        </v-icon>
+        计分规则
+      </v-btn>
+      <v-btn class="mx-2" dark color="green darken-1">
+        <v-icon left dark>
+          mdi-plus
+        </v-icon>
+        加桌
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <Table />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Table from "./components/Table";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Table,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Table.vue
@@ -0,0 +1,117 @@
+<template>
+  <v-container>
+    <v-card tile width="100%" class="green lighten-1 text-center mb-6">
+      <v-card-text class="white--text"
+        ><strong>三国杀国战裁判计分器</strong>
+      </v-card-text>
+    </v-card>
+    <v-data-table :headers="headers" :items="desserts" hide-default-footer>
+      <template v-slot:item.name="props">
+        <v-text-field
+          v-model="props.item.name"
+          label="请输入选手ID"
+          single-line
+        ></v-text-field>
+      </template>
+      <template v-slot:item.iron="props">
+        <v-select
+          v-model="props.item.iron"
+          :items="countries"
+          label="请选择国家"
+          align="center"
+          justify="center"
+        >
+          <template #selection="{ item }">
+            <v-chip :color="countryColor(item)" text-color="white">{{
+              item
+            }}</v-chip>
+          </template>
+        </v-select>
+      </template>
+      <template v-slot:item.optionA="props">
+        <v-simple-checkbox v-model="props.item.optionA"></v-simple-checkbox>
+      </template>
+    </v-data-table>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      countries: ["魏国", "蜀国", "吴国", "群雄", "野心家"],
+      headers: [
+        {
+          text: "Dessert (100g serving)",
+          align: "start",
+          sortable: false,
+          value: "name",
+        },
+        { text: "Calories", value: "calories" },
+        { text: "Iron (%)", value: "iron" },
+        { text: "optionA", value: "optionA" },
+      ],
+      desserts: [
+        {
+          name: "",
+          calories: 159,
+          iron: "1%",
+          optionA: false,
+        },
+        {
+          name: "",
+          calories: 159,
+          iron: "1%",
+          optionA: false,
+        },
+        {
+          name: "",
+          calories: 159,
+          iron: "1%",
+          optionA: false,
+        },
+        {
+          name: "",
+          calories: 159,
+          iron: "1%",
+          optionA: false,
+        },
+        {
+          name: "",
+          calories: 159,
+          iron: "1%",
+          optionA: false,
+        },
+        {
+          name: "",
+          calories: 159,
+          iron: "1%",
+          optionA: false,
+        },
+        {
+          name: "",
+          calories: 159,
+          iron: "1%",
+          optionA: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    countryColor(country) {
+      switch (country) {
+        case "魏国":
+          return "blue";
+        case "蜀国":
+          return "red";
+        case "吴国":
+          return "green";
+        case "群雄":
+          return "grey";
+        default:
+          return "purple";
+      }
+    },
+  },
+};
+</script>
